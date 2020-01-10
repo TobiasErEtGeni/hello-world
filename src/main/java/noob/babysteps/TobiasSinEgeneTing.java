1972fad28b988659c8ecc21fbe148f20e2512e44
Bauhr Hans Petter H <hanbau@vegvesen.no>
2020-01-10 15:20:44 +0100

    kalkulator

--- src/main/java/noob/babysteps/TobiasSinEgeneTing.java
+++ src/main/java/noob/babysteps/TobiasSinEgeneTing.java
@@ -3,26 +3,28 @@ package noob.babysteps;
 import java.util.Scanner;
 
 class TobiasSinEgeneTing {
+    String string = new String("dfdfg");
 
     public static void main(String[] arguments) {
         System.out.println(new TobiasSinEgeneTing().jada());
     }
 
-    float jada() {
-        Scanner scanner = new Scanner(System.in);
+    Integer jada() {
+        Scanner myScanner = new Scanner(System.in);
         System.out.println("1 for pluss kalkulator, 2 for multiplikajons kalkulator");
-        Integer plussMul = scanner.nextInt();
+        Integer plussMul = myScanner.nextInt();
 
         if (plussMul != 1 && plussMul != 2 ) {
             System.out.println("Jeg gir opp :-(");
+            myScanner.close();
             return 0;
         }
 
         System.out.println("Skriv inn et tall");
-        Integer tall1 = scanner.nextInt();
+        Integer tall1 = myScanner.nextInt();
         System.out.println("og et tall til");
-        Integer tall2 = scanner.nextInt();
-        scanner.close();
+        Integer tall2 = myScanner.nextInt();
+        myScanner.close();
 
         if (plussMul == 2) {
             System.out.println(tall1 + " ganger " + tall2 + " er lik:");
