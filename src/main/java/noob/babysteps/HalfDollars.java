9dcb3b50febb8e33efc04c917c82b3cf71cc9683
Bauhr Hans Petter H <hanbau@vegvesen.no>
2020-01-10 15:18:05 +0100

    take userinput and calculate average

--- src/main/java/noob/babysteps/HalfDollars.java
+++ src/main/java/noob/babysteps/HalfDollars.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 class HalfDollars {
     int[] input = new int[42];
     int average;
+    int counter = 0;
     Scanner myScanner = new Scanner(System.in);
 
     public static void main(String[] arguments) {
@@ -15,12 +16,13 @@ class HalfDollars {
 
     private void getUserinput() {
         boolean haveUserInput = true;
-        int counter = 0;
+
         while (haveUserInput) {
-            System.out.println("Hello");
+            System.out.println("Type your " + (counter + 1) + ". Number");
+            System.out.println("If you want to submit type submit");
             String userinput = myScanner.next();
 
-            if (userinput.equalsIgnoreCase("q")) {
+            if (userinput.equalsIgnoreCase("submit")) {
                 haveUserInput = false;
                 break;
             }
@@ -28,27 +30,16 @@ class HalfDollars {
             input[counter] = Integer.valueOf(userinput);
             counter = counter + 1; // counter++
 
-            System.out.println(
-                    "you now have " + input[0] + " and " + input[1] + " and " + input[2] + " amounts of money");
-
         }
-/*
-        System.out.print("Welcome to the money average booth, please type in your first number");
-        String userinput = myScanner.next();
-        System.out.println("you have chosen " + userinput + " amounts of money");
-        input[0] = Integer.valueOf(userinput);
-        System.out.println("you now have " + input[0] + " amounts of money");
-        System.out.println("Please Put in another number");
-        userinput = myScanner.next();
-        input[1] = Integer.valueOf(userinput);
-        System.out.println("you now have " + (input[0] + input[1]) + " amounts of money");
-        System.out
-                .println("Put in another number, if you wish to calculate your average now or later type in Calculate");
-*/
     }
 
     private void average() {
-        average = (input[0] + input[1] + input[2]) / 3;
+        int sum = 0;
+        for (int i = 0; i < counter; ++i) {
+            sum = sum + input[i];
+        }
+
+        average = sum / counter;
         System.out.println("This is your average: " + average);
     }
 
