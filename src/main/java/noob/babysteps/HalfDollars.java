8d8466af9b5227d9fa8d640d70196397604e0b67
Bauhr Hans Petter H <hanbau@vegvesen.no>
2020-01-10 13:42:18 +0100

    Average calculator thingy

--- src/main/java/noob/babysteps/HalfDollars.java
+++ src/main/java/noob/babysteps/HalfDollars.java
@@ -1,46 +1,55 @@
 package noob.babysteps;
 
+import java.util.Scanner;
+
 class HalfDollars {
-    int[] denver = { 2_100_000, 2_900_000, 6_100_000 };
-    int[] philadelphia = new int[denver.length];
-    int[] total = new int[denver.length];
+    int[] input = new int[42];
     int average;
+    Scanner myScanner = new Scanner(System.in);
 
     public static void main(String[] arguments) {
         HalfDollars kake = new HalfDollars();
-        kake.calculateTotalYearOne();
-        kake.calculateTotalYearTwo();
-        kake.calculateTotalYearThree();
-        kake.Average();
+        kake.getUserinput();
+        kake.average();
     }
 
-    private void calculateTotalYearOne() {
+    private void getUserinput() {
+        boolean haveUserInput = true;
+        int counter = 0;
+        while (haveUserInput) {
+            System.out.println("Hello");
+            String userinput = myScanner.next();
 
-        philadelphia[0] = 2_100_000;
-        total[0] = denver[0] + philadelphia[0];
-        System.out.print("2016 production: ");
-        System.out.format("%,d%n", total[0]);
+            if (userinput.equalsIgnoreCase("q")) {
+                haveUserInput = false;
+                break;
+            }
+
+            input[counter] = Integer.valueOf(userinput);
+            counter = counter + 1; // counter++
+
+            System.out.println(
+                    "you now have " + input[0] + " and " + input[1] + " and " + input[2] + " amounts of money");
+
+        }
+/*
+        System.out.print("Welcome to the money average booth, please type in your first number");
+        String userinput = myScanner.next();
+        System.out.println("you have chosen " + userinput + " amounts of money");
+        input[0] = Integer.valueOf(userinput);
+        System.out.println("you now have " + input[0] + " amounts of money");
+        System.out.println("Please Put in another number");
+        userinput = myScanner.next();
+        input[1] = Integer.valueOf(userinput);
+        System.out.println("you now have " + (input[0] + input[1]) + " amounts of money");
+        System.out
+                .println("Put in another number, if you wish to calculate your average now or later type in Calculate");
+*/
     }
 
-    private void calculateTotalYearTwo() {
-        philadelphia[1] = 1_800_000;
-        total[1] = denver[1] + philadelphia[1];
-        System.out.print("2017 production: ");
-        System.out.format("%,d%n", total[1]);
-    }
-
-    private void calculateTotalYearThree() {
-
-        philadelphia[2] = 4_800_000;
-        total[2] = denver[2] + philadelphia[2];
-        System.out.print("2018 production: ");
-        System.out.format("%,d%n", total[2]);
-    }
-
-    private void Average() {
-        average = (total[0] + total[1] + total[2]) / 3;
-        System.out.print("Average production: ");
-        System.out.format("%,d%n", average);
+    private void average() {
+        average = (input[0] + input[1] + input[2]) / 3;
+        System.out.println("This is your average: " + average);
     }
 
 }
